e1c1705b261ecbbe94fbdb8b25d45c583ef18697
devops-intellipaat <hemant@intellipaat.com>
2019-01-18 02:28:17 +0000

    added feature A

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
-pseudo code
+upseudo code
 {
 Initial Function()
 {
   //Sample Code
 }
 
+<<<<<<< HEAD
 Initial Function2()
 {
  //Sample Code
+
+feature1()
+{
+  //Sample Code for Feature 1
+// added feature A
 }
 
 
