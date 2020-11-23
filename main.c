b3777a586b77f6611118d5bbfe8e05786457d695
satyatripathy1989 <71309024+satyatripathy1989@users.noreply.github.com>
2020-11-23 14:05:32 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,12 +2,13 @@ pseudo code
 {
 Initial Function()
 {
-  //Sample Code
+  //Sample Code Feature 1
 }
 
 feature1()
 {
-  //Sample Code for Feature 1
+  //Sample Code for Feature 1 update
+  
 }
 
 
