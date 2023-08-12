d8532f2e7b2b042a52369c9e0ccf4229369c3b65
Dilpreet <Dilpreetkaur2001@yahoo.co.in>
2023-08-12 05:31:06 +0000

    Security patch

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
+Code added from master branch
+
+{
+//Sample Code	
+}
+
 pseudo code
 {
 Initial Function()
 {
-  //Sample Code
+
+      	//Sample Code added from feature 1 branch
 }
 
 Initial Security()
 {
- //Sample Code
+ //Sample Code added from feature 2 branch
 }
 
 
